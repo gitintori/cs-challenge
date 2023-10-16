4682f99ac315f73a40488102d4a945b0ace1fbac
Giovanna Tintori <giovannatintori@hotmail.com>
2023-10-16 11:55:25 -0300

    Alterar ordem de estrelas do repo

--- src/pages/Repos/Repos.tsx
+++ src/pages/Repos/Repos.tsx
@@ -3,38 +3,61 @@ import { useState, useEffect } from "react";
 
 const url = 'https://api.github.com/';
 
+type Repository = {
+  id: number;
+  name: string;
+  stargazers_count: number;
+};
+
 const Repos = () => {
   const { id } = useParams();
 
-  const [repos, setRepos] = useState([]);
+  const [repos, setRepos] = useState<Repository[]>([]);
+  const [ordenacao, setOrdenacao] = useState('desc');
 
   useEffect(() => {
     const getRepos = async () => {
-      const resposta = await fetch(`${url}users/${id}/repos`);
-      console.log(resposta)
-      const dados = await resposta.json();
+      try {
+        const resposta = await fetch(`${url}users/${id}/repos`);
+        if (!resposta.ok) {
+          throw new Error(`Erro na requisição: ${resposta.status}`);
+        }
+        const dados: Repository[] = await resposta.json();
+        
+        const reposOrdenados = [...dados];
+          if (ordenacao === 'asc') {
+            reposOrdenados.sort((a, b) => a.stargazers_count - b.stargazers_count);
+          } else {
+            reposOrdenados.sort((a, b) => b.stargazers_count - a.stargazers_count);
+          }
 
-      dados.sort((a: { stargazers_count: number }, b: { stargazers_count: number }) => b.stargazers_count - a.stargazers_count);
-
-
-      setRepos(dados);
-      console.log(dados)
-      
-    }
+        setRepos(reposOrdenados);
+      } catch (error) {
+        console.error('Erro na requisição:', error);
+      }
+    };
 
     getRepos();
-  }, [id]);
+  }, [id, ordenacao]);
+
+  const handleOrdernar = (ordem: 'asc' | 'desc') => {
+    setOrdenacao(ordem);
+  };
 
   return (
     <div>
         <Link to={`/users/${id}`}>
                 Voltar
-            </Link>
-        <ul>
-            {repos.map((repo: any) => (
-                    <li key={repo.id}>{repo.name} - Estrelas: {repo.stargazers_count}</li>
-                ))}
-        </ul>
+        </Link>
+      <div>
+        <button onClick={() => handleOrdernar('asc')}>Ordem Crescente</button>
+        <button onClick={() => handleOrdernar('desc')}>Ordem Decrescente</button>
+      </div>
+      <ul>
+          {repos.map((repo: any) => (
+                  <li key={repo.id}>{repo.name} - Estrelas: {repo.stargazers_count}</li>
+              ))}
+      </ul>
     </div>
   );
 };
